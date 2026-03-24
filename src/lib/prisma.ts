73fdf263be0c423c0dce1cbe14e752630e43aeda
George Gomes <georgegomesf@gmail.com>
2026-03-24 05:12:12 -0300

    refactor: remove explicit pg adapter configuration for PrismaClient

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,11 @@
 import { PrismaClient } from '@prisma/client'
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
 function createPrismaClient() {
-  const pool = new Pool({ connectionString: process.env.DATABASE_URL! })
-  // @ts-ignore - Ignore type incompatibility between pg and Prisma adapter
-  const adapter = new PrismaPg(pool)
-  return new PrismaClient({ adapter })
+  return new PrismaClient()
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient()
