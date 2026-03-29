31eb64bdcb6d6029baaec88cb02879702bf8a5f3
George Gomes <georgegomesf@gmail.com>
2026-03-29 18:49:34 -0300

    refactor: remove obsolete administrative scripts and temporary database migration tools

--- check_cats.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-async function main() {
-  const categories = await prisma.category.findMany({
-    where: { slug: { in: ['revistas', 'edicoes', 'artigos'] } }
-  })
-  console.log(JSON.stringify(categories, null, 2))
-}
-main().catch(console.error).finally(() => prisma.$disconnect())
