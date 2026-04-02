e5161a8263fa0305ce62d00aefcf238d188bf4e0
George Gomes <georgegomesf@gmail.com>
2026-04-02 09:46:00 -0300

    refactor: migrate authentication middleware to proxy module

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
 
-export default auth;
+export const proxy = auth;
+export default proxy;
 
 export const config = {
     matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
